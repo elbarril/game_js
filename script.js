e14c660d9c48e5802760b3b5f9831e21cfc62b97
BrisaND <brisadesouches@gmail.com>
2024-06-11 18:58:41 -0300

    first commit: small change

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function loadMap(map){
     let gameElement = document.body.querySelector('#game');
     for (let row = 0; row < map.length; row++) {
-        const items = map[row];
+        const itemsMap = map[row];
         let rowElement = document.createElement('ul');
-        for (let column = 0; column < items.length; column++) {
-            const item = items[column];
+        for (let column = 0; column < itemsMap.length; column++) {
+            const item = itemsMap[column];
             let itemElement = createItem(item, row, column);
             itemElement && rowElement.appendChild(itemElement);
         }
@@ -12,30 +12,23 @@ function loadMap(map){
     }
 }
 
-const ITEMS = {
+const ITEMS_MAP = {
     "0": false,
     "1": "solid",
     "2": "player"
 }
 
-const DIRECTION_KEYS = {
-    "ArrowUp": "UP",
-    "ArrowDown": "DOWN",
-    "ArrowLeft": "LEFT",
-    "ArrowRight": "RIGHT"
-}
-
 const MOVEMENTS = {
-    "UP": [-1, 0],
-    "DOWN": [1, 0],
-    "LEFT": [0, -1],
-    "RIGHT": [0, 1]
+    "ArrowUp": [-1, 0],
+    "ArrowDown": [1, 0],
+    "ArrowLeft": [0, -1],
+    "ArrowRight": [0, 1]
 }
 
 function createItem(itemCode, row, column){
     try {
         let itemElement = document.createElement('li');
-        ITEMS[itemCode] && itemElement.classList.add(ITEMS[itemCode]);
+        ITEMS_MAP[itemCode] && itemElement.classList.add(ITEMS_MAP[itemCode]);
         itemElement.dataset["row"] = row;
         itemElement.dataset["column"] = column;
         itemElement.dataset["id"] = itemCode;
@@ -48,7 +41,7 @@ function createItem(itemCode, row, column){
 function createControls() {
     document.addEventListener('keydown', event => {
         try {
-            const direction = DIRECTION_KEYS[event.key];
+            const direction = MOVEMENTS[event.key];
             move(direction);
         } catch (error) {
             console.log(error);
@@ -57,12 +50,11 @@ function createControls() {
 }
 
 function move(direction) {
-    let movement = MOVEMENTS[direction];
     let player = document.querySelector(".player");
     let row = new Number(player.dataset.row);
     let column = new Number(player.dataset.column);
-    let newRow = row + new Number(movement[0]);
-    let newColumn = column + new Number(movement[1]);
+    let newRow = row + new Number(direction[0]);
+    let newColumn = column + new Number(direction[1]);
 
     let positionToMove = document.querySelector(`[data-row="${newRow}"][data-column="${newColumn}"]`);
     if (positionToMove && ITEMS[positionToMove.dataset.id] != "solid") {

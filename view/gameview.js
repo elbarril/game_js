e11ef5436b90ea5935d310b03cfd84ed9d53e5da
BrisaND <brisadesouches@gmail.com>
2024-06-24 20:26:48 -0300

    create the view; set title, render the map and update it

---
+++ view/gameview.js
@@ -0,0 +1,46 @@
+class GameView{
+    game = document.querySelector('#game')
+    title;
+    map;
+
+    constructor(){
+        this.title = document.createElement('h2');
+        this.map = document.createElement('div');
+
+        this.game.appendChild(this.title);
+        this.game.appendChild(this.map);
+    }
+    
+    setTitle(title){
+        this.title.innerText = title;
+    }
+
+    renderMap(positions){
+
+        for (let row = 0; row < positions.length; row++) {
+            let mapRow = document.createElement('ul');
+            for (let column = 0; column < positions[row].length; column++) {
+                let mapColumn = document.createElement('li');
+                mapColumn.innerText = positions[row][column];
+                mapRow.appendChild(mapColumn);
+            }
+            this.map.appendChild(mapRow);
+        }
+        
+    }
+
+    updateMap(positions){
+
+        let row = 0;
+        document.querySelectorAll('ul').forEach(mapRow => {
+            let column = 0;
+            mapRow.querySelectorAll('li').forEach(mapColumn =>{
+                mapColumn.innerText = positions[row][column];
+                column++;
+            });
+            row++;
+        });
+    }
+}
+
+export default GameView;

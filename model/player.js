a8168fa5bcdbea88f9597e1018eb80742cec56e6
BrisaND <brisadesouches@gmail.com>
2024-06-24 20:22:55 -0300

    method to move the player + coonstructor

---
+++ model/player.js
@@ -0,0 +1,25 @@
+class Player{
+
+    character = null;
+    name;
+
+    constructor(name){
+        this.name=name;
+    }
+
+    moveCharacter(map, direction){
+
+        this.character.direction = direction;
+        let nextX = this.character.position.x + direction.x;
+        let nextY =this.character.position.y + direction.y;
+        if (map.positions.length < nextY || nextY < 0 ) return;
+        if (map.positions[nextY].length < nextX || nextX < 0 ) return;
+        if (map.positions[nextX][nextY]) return;
+
+        map.remove(this.character);
+        this.character.move();
+        map.add(this.character);
+    }
+}
+
+export default Player;

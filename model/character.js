15a73ab386dc991dbda58d59d54286450721265a
BrisaND <brisadesouches@gmail.com>
2024-06-30 18:00:45 -0300

    refact Character

--- model/character.js
+++ model/character.js
@@ -1,17 +1,23 @@
-import MapItem from './mapitem.js'
+import MovableMapItem from './movablemapitem.js';
+import {UP_DIRECTION, DOWN_DIRECTION, LEFT_DIRECTION, RIGHT_DIRECTION} from './MOVEMENTS.js';
 
-class Character extends MapItem{
+class Character extends MovableMapItem{
 
-    move(){
+    constructor(position, direction=UP_DIRECTION){
+        super(position, direction);
+    }
 
-        if (!this.direction) return;
-        this.position.x += this.direction.x;
-        this.position.y += this.direction.y;
+    say(message){
+        alert(message);
     }
 
     toString(){
 
-        return '*';
+        if (this.direction === UP_DIRECTION) return 'UP';
+        else if (this.direction === DOWN_DIRECTION) return 'DOWN';
+        else if (this.direction === LEFT_DIRECTION) return 'LEFT';
+        else if (this.direction === RIGHT_DIRECTION) return 'RIGHT';
+
     }
 
 }

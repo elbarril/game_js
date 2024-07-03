8db789638ad48aa6c584e3f684d12255166a2657
BrisaND <brisadesouches@gmail.com>
2024-07-03 19:36:53 -0300

    change action of interactable object

--- model/interactable.js
+++ model/interactable.js
@@ -2,14 +2,8 @@ import MapItem from "./mapitem.js";
 
 class Interactable extends MapItem{
 
-    interactPlayer(player){
-        let nextX = player.character.position.x + player.character.direction.x;
-        let nextY = player.character.position.y + player.character.direction.y;
-        if (nextX === this.position.x && nextY === this.position.y){
-            if (confirm('you wanna go?')){
-                return true;
-            }
-        }
+    action(){
+        return confirm('you wanna go?');
     }
 
     toString(){

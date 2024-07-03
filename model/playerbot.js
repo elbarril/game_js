128255a2339fa6d83c06f42f0f67e4eb52befe35
BrisaND <brisadesouches@gmail.com>
2024-07-03 19:38:48 -0300

    remove user interaction in PlayerBot

--- model/playerbot.js
+++ model/playerbot.js
@@ -10,19 +10,6 @@ class PlayerBot extends Player{
         super.moveCharacter(map, randomDirection);
     }
 
-    interactWithPlayer(player){
-
-        document.addEventListener('keydown', event => {
-            let nextX = player.character.position.x + player.character.direction.x;
-            let nextY = player.character.position.y + player.character.direction.y;
-            if (nextX === this.character.position.x && nextY === this.character.position.y && event.key === 'e'){
-                this.character.say('Hi');
-            }
-            
-        });
-    
-    }
-
 }
 
 export default PlayerBot;
